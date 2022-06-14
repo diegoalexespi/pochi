f21cf3786fcadf8e9f48b1990cb9ed9ee69e3122
diegoalexespi <d93espinoza@gmail.com>
2022-06-14 12:58:28 -0400

    adding group.singletons to Leiden

--- R/FindClustersLeiden.R
+++ R/FindClustersLeiden.R
@@ -2,9 +2,13 @@
 #'
 #' @param object Seurat object
 #' @param graph.name Name of Graph slot in object to use for Leiden clustering
+#' @param group.singletons Group singletons into nearest cluster. If FALSE, the
+#' clusters will remain as single-member clusters.
 #' @param resolution Vector of resolution to input into Leiden
-#' @param min.cluster.size Minimum cluster size trimmed after clustering
+#' @param min.cluster.size Minimum cluster size trimmed after clustering (not
+#' currently used)
 #' @param n.iterations Number of iterations to run Leiden clustering for
+#' @param verbose Control verbosity
 #' @param pythondir Specified director for python binary that has the leidenalg
 #' library installed
 #' @param partition_type The partition_type to specify for Leiden.
@@ -23,17 +27,20 @@
 #' @references code is adapted from @immunogenomics/singlecellmethods
 #'
 #' @importFrom rlang %||%
+#' @importFrom magrittr %>%
+#' @importFrom dplyr left_join
 #'
 #' @export
 FindClustersLeiden <- function(object,
                                graph.name = NULL,
+                               group.singletons = TRUE,
                                resolution = c(0.8),
                                min.cluster.size = 5,
                                n_iterations = 2,
+                               verbose = TRUE,
                                pythondir = "~/miniconda3/bin/python",
                                partition_type = "RBConfigurationVertexPartition") {
 
-  message("Warning: singleton grouping not implemented yet")
   message("Loading reticulate")
   require(reticulate)
   reticulate::use_python(pythondir)
@@ -65,7 +72,6 @@ FindClustersLeiden <- function(object,
   #read it in using Read_Ncol
   g <- ig$Graph$Read_Ncol(tmpfile, names = TRUE, weights = "if_present", directed = FALSE)
 
-
   .res_list <- lapply(resolution, function(resolution) {
     message("Applying Leiden algorithm for resolution: ", resolution)
     cres <- leiden$find_partition(g,
@@ -73,21 +79,40 @@ FindClustersLeiden <- function(object,
                                   resolution_parameter = resolution,
                                   n_iterations = n_iterations)
     cres_membership <- data.frame(nodes = cres$graph$vs["name"], membership = cres$membership)
-    clusters_keep <- names(table(cres_membership$membership))[which(table(cres_membership$membership) >= min.cluster.size)]
-    cres_membership$membership[which(!cres_membership$membership %in% clusters_keep)] <- NA
-    n_na <- sum(is.na(cres_membership$membership))
-    message(sprintf("Resolution %f yielded %d clusters", resolution, length(clusters_keep)))
-    if (n_na > 0) message(sprintf("WARNING: for resolution %f, %d/%d points removed from small clusters", resolution, n_na, length(cres_membership$membership)))
-    colnames(cres_membership)[2] <- paste0("leiden_res.", resolution)
-    return(cres_membership)
+
+    ids <- cres_membership$membership
+    names(ids) <- cres_membership$nodes
+    ids <- ids[colnames(object)]
+
+    if(group.singletons){
+      message("Grouping singletons")
+      ids <- Seurat:::GroupSingletons(ids = ids, SNN = snn, group.singletons = group.singletons, verbose = verbose)
+    } else {
+      message(sprintf("Not grouping %d singletons", sum(table(ids) == 1)))
+    }
+    clustering_results <- data.frame(nodes = names(ids), ids = ids)
+    colnames(clustering_results)[2] <- paste0(graph.name, ".leiden_res.", resolution)
+
+    ### comment this code out but it is for setting a minimum cluster size
+    # clusters_keep <- names(table(cres_membership$membership))[which(table(cres_membership$membership) >= min.cluster.size)]
+    # cres_membership$membership[which(!cres_membership$membership %in% clusters_keep)] <- NA
+    # n_na <- sum(is.na(cres_membership$membership))
+    # message(sprintf("Resolution %f yielded %d clusters", resolution, length(clusters_keep)))
+    # if (n_na > 0){
+    #   message(sprintf("WARNING: for resolution %f, %d/%d points removed from small clusters", resolution, n_na, length(cres_membership$membership)))
+    # }
+    ###
+
+    return(clustering_results)
   })
+
   .res <-  .res_list %>%
     purrr::reduce(left_join, by = "nodes")
 
   message("Finished leiden clustering")
   rownames(.res) <- .res$nodes
   .res$nodes <- NULL
-  .res <- .res[colnames(object),]
+  .res <- .res[colnames(object),,drop=FALSE]
 
   object <- AddMetaData(object = object, metadata = .res)
   return(object)
