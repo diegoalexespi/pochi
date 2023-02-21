fb7bd14b142494b2d6b4eaa0a504d6b2b177650d
diegoalexespi <d93espinoza@gmail.com>
2023-02-21 16:48:20 -0500

    adding dissociation.genes to package

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Dissociation-associated gene signature
+#'
+#' The human gene names of the dissociation-associated gene signatures from the
+#' "Single-cell sequencing reveals dissociation-induced gene expression in
+#' tissue subpopulations." van den Brink SC, et al. Nat Methods 2017.
+#'
+#' @format ## `dissociation.genes`
+#' A vector with 137 gene names:
+#' \describe{
+#'   \item{gene name}{gene name}
+#'   ...
+#' }
+#' @source <https://www.nature.com/articles/nmeth.4437>
+"dissociation.genes"
