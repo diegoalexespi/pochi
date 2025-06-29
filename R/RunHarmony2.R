42d408ba086e91252775ec954524cd4a052159a7
diegoalexespi <d93espinoza@gmail.com>
2025-06-29 13:58:09 -0400

    Adding RunHarmonyForSymphony

--- R/RunHarmony2.R
+++ R/RunHarmony2.R
@@ -3,113 +3,80 @@
 #' @return Runs Harmony in the implementation of Symphony such that the Harmony
 #' model is stored within the Seurat object for later use with Symphony, see
 #' https://github.com/immunogenomics/symphony/blob/main/vignettes/utils_seurat.R
-#' @references copy-pasted from @immunogenomics/symphony github repo
+#' @references copy-pasted from @immunogenomics/harmony github repo
 #'
 #' @importFrom rlang %||%
 #'
 #' @export
-RunHarmony2 <- function(
-  object,
-  group.by.vars,
-  reduction = 'pca',
-  dims.use = NULL,
-  theta = NULL,
-  lambda = NULL,
-  sigma = 0.1,
-  nclust = NULL,
-  tau = 0,
-  block.size = 0.05,
-  max.iter.harmony = 10,
-  max.iter.cluster = 20,
-  epsilon.cluster = 1e-5,
-  epsilon.harmony = 1e-4,
-  plot_convergence = FALSE,
-  verbose = TRUE,
-  reference_values = NULL,
-  reduction.save = "harmony",
-  assay.use = 'RNA',
-  project.dim = TRUE,
-  ...
+RunHarmonyForSymphony <- function(
+    object,
+    group.by.vars,
+    reduction.use = 'pca',
+    dims.use = NULL,
+    reduction.save = "harmony",
+    project.dim = TRUE,
+    ...
 ) {
-  if (reduction == "pca") {
-    tryCatch(
-      embedding <- Seurat::Embeddings(object, reduction = "pca"),
-      error = function(e) {
-        if (verbose) {
-          message("Harmony needs PCA. Trying to run PCA now.")
-        }
-        tryCatch(
-          object <- Seurat::RunPCA(
-            object,
-            assay = assay.use, verbose = verbose
-          ),
-          error = function(e) {
-            stop("Harmony needs PCA. Tried to run PCA and failed.")
-          }
-        )
-      }
-    )
-  } else {
-    available.dimreduc <- names(methods::slot(object = object, name = "reductions"))
-    if (!(reduction %in% available.dimreduc)) {
-      stop("Requested dimension reduction is not present in the Seurat object")
-    }
-    embedding <- Seurat::Embeddings(object, reduction = reduction)
+
+  if (!requireNamespace('Seurat', quietly = TRUE)) {
+    stop("Running Harmony on a Seurat object requires Seurat")
   }
+  if (!reduction.use %in% Seurat::Reductions(object = object)) {
+    stop(paste(reduction.use, "cell embeddings not found in Seurat object.",
+               "For a Seurat preprocessing walkthrough, please refer to the vignette"))
+  }
+  embedding <- Seurat::Embeddings(object, reduction = reduction.use)
   if (is.null(dims.use)) {
     dims.use <- seq_len(ncol(embedding))
   }
   dims_avail <- seq_len(ncol(embedding))
   if (!all(dims.use %in% dims_avail)) {
-    stop("trying to use more dimensions than computed. Rereun dimension reduction
+    stop("trying to use more dimensions than computed. Rerun dimension reduction
          with more dimensions or run Harmony with fewer dimensions")
   }
   if (length(dims.use) == 1) {
     stop("only specified one dimension in dims.use")
   }
-  metavars_df <- Seurat::FetchData(object, group.by.vars)
-
-  harmonyObject <- HarmonyMatrix(
-    embedding,
-    metavars_df,
+  metavars_df <- Seurat::FetchData(
+    object,
     group.by.vars,
-    FALSE,
-    0,
-    theta,
-    lambda,
-    sigma,
-    nclust,
-    tau,
-    block.size,
-    max.iter.harmony,
-    max.iter.cluster,
-    epsilon.cluster,
-    epsilon.harmony,
-    plot_convergence,
-    TRUE,
-    verbose,
-    reference_values
+    cells = Seurat::Cells(x = object[[reduction.use]])
   )
 
-  harmonyEmbed <- t(as.matrix(harmonyObject$Z_corr))
-  rownames(harmonyEmbed) <- row.names(embedding)
-  colnames(harmonyEmbed) <- paste0(reduction.save, "_", seq_len(ncol(harmonyEmbed)))
+  #changes here
+  ####
+  data_mat <- embedding[,dims.use] #change #1
+  data_mat <- Matrix::t(data_mat) #change #2 need to do this because
+  # RunHarmony.default does this anyways and we want data_mat to be ready to be
+  # used to add the row and col names
+  harmonyObject <- harmony::RunHarmony(
+    data_mat = data_mat,
+    meta_data = metavars_df,
+    vars_use = group.by.vars,
+    return_object = TRUE, #this is the big change, #3
+    ...
+  )
 
-  harmonyClusters <- t(harmonyObject$R)
-  rownames(harmonyClusters) <- row.names(embedding)
-  colnames(harmonyClusters) <- paste0('R', seq_len(ncol(harmonyClusters)))
+  # #this is needed beacuse we need to extract the embeddings for the object
+  result_matrix <- as.matrix(harmonyObject$Z_corr)
+  row.names(result_matrix) <- row.names(data_mat)
+  colnames(result_matrix) <- colnames(data_mat)
+  harmonyEmbed <- t(result_matrix)
+  # #end extraction
 
-  suppressWarnings({
-    harmonydata <- Seurat::CreateDimReducObject(
-      embeddings = harmonyEmbed,
-      stdev = as.numeric(apply(harmonyEmbed, 2, stats::sd)),
-      assay = assay.use,
-      key = reduction.save,
-      misc=list(R=harmonyClusters)
-    )
-  })
+  ####
 
-  object[[reduction.save]] <- harmonydata
+  reduction.key <- Seurat::Key(reduction.save, quiet = TRUE)
+  rownames(harmonyEmbed) <- rownames(embedding)
+  colnames(harmonyEmbed) <- paste0(reduction.key, seq_len(ncol(harmonyEmbed)))
+
+  object[[reduction.save]] <- Seurat::CreateDimReducObject(
+    embeddings = harmonyEmbed,
+    stdev = as.numeric(apply(harmonyEmbed, 2, stats::sd)),
+    assay = Seurat::DefaultAssay(object = object[[reduction.use]]),
+    key = reduction.key,
+    misc = list(harmony_object = harmonyObject)#another change
+  )
   if (project.dim) {
     object <- Seurat::ProjectDim(
       object,
@@ -120,7 +87,4 @@ RunHarmony2 <- function(
   }
   return(object)
 }
-
-
-
 
